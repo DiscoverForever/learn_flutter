84dd3e8385555ba5143d3610ebc2efecb450b007
discoverforever <yueyangtang@163.com>
2019-07-24 21:00:14 +0800

    Add welcome bean

---
+++ lib/bean/home/welcome_home.dart
@@ -0,0 +1,533 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'welcome_home.g.dart';
+
+@JsonSerializable()
+class WelcomeHome extends Object {
+  @JsonKey(name: 'naviVer')
+  String naviVer;
+
+  @JsonKey(name: 'toTopBtnImg')
+  String toTopBtnImg;
+
+  @JsonKey(name: 'configDisplayVersion')
+  String configDisplayVersion;
+
+  @JsonKey(name: 'userCategory')
+  String userCategory;
+
+  @JsonKey(name: 'executeTime')
+  int executeTime;
+
+  @JsonKey(name: 'topBgImgBig')
+  String topBgImgBig;
+
+  @JsonKey(name: 'personalSourceValue')
+  String personalSourceValue;
+
+  @JsonKey(name: 'popMessage')
+  int popMessage;
+
+  @JsonKey(name: 'searchBgColor')
+  String searchBgColor;
+
+  @JsonKey(name: 'displayVersion')
+  String displayVersion;
+
+  @JsonKey(name: 'tagAnimations')
+  int tagAnimations;
+
+  @JsonKey(name: 'showLightIcon')
+  int showLightIcon;
+
+  @JsonKey(name: 'canBeDefault')
+  bool canBeDefault;
+
+  @JsonKey(name: 'loginText')
+  String loginText;
+
+  @JsonKey(name: 'floorList')
+  List<FloorList> floorList;
+
+  @JsonKey(name: 'toBottomBtnImg')
+  String toBottomBtnImg;
+
+  @JsonKey(name: 'cycNum')
+  int cycNum;
+
+  @JsonKey(name: 'use_new_brands')
+  int useNewBrands;
+
+  @JsonKey(name: 'alignedCurrentTime')
+  int alignedCurrentTime;
+
+  @JsonKey(name: 'cycFirstTimeStamp')
+  String cycFirstTimeStamp;
+
+  @JsonKey(name: 'searchBgImg')
+  String searchBgImg;
+
+  @JsonKey(name: 'code')
+  String code;
+
+  @JsonKey(name: 'appCenterAnimations')
+  int appCenterAnimations;
+
+  @JsonKey(name: 'skinSwitch')
+  int skinSwitch;
+
+  @JsonKey(name: 'lottieCartoonList')
+  List<dynamic> lottieCartoonList;
+
+  @JsonKey(name: 'topIconText')
+  int topIconText;
+
+  @JsonKey(name: 'useComponentImg')
+  int useComponentImg;
+
+  @JsonKey(name: 'topBgImgBigColor')
+  String topBgImgBigColor;
+
+  @JsonKey(name: 'appCenterCartoonSwitch')
+  int appCenterCartoonSwitch;
+
+  @JsonKey(name: 'topBgImg')
+  String topBgImg;
+
+  @JsonKey(name: 'recommendAB')
+  String recommendAB;
+
+  @JsonKey(name: 'lazy')
+  int lazy;
+
+  @JsonKey(name: 'webViewFloorList')
+  List<WebViewFloorList> webViewFloorList;
+
+  WelcomeHome(
+    this.naviVer,
+    this.toTopBtnImg,
+    this.configDisplayVersion,
+    this.userCategory,
+    this.executeTime,
+    this.topBgImgBig,
+    this.personalSourceValue,
+    this.popMessage,
+    this.searchBgColor,
+    this.displayVersion,
+    this.tagAnimations,
+    this.showLightIcon,
+    this.canBeDefault,
+    this.loginText,
+    this.floorList,
+    this.toBottomBtnImg,
+    this.cycNum,
+    this.useNewBrands,
+    this.alignedCurrentTime,
+    this.cycFirstTimeStamp,
+    this.searchBgImg,
+    this.code,
+    this.appCenterAnimations,
+    this.skinSwitch,
+    this.lottieCartoonList,
+    this.topIconText,
+    this.useComponentImg,
+    this.topBgImgBigColor,
+    this.appCenterCartoonSwitch,
+    this.topBgImg,
+    this.recommendAB,
+    this.lazy,
+    this.webViewFloorList,
+  );
+
+  factory WelcomeHome.fromJson(Map<String, dynamic> srcJson) =>
+      _$WelcomeHomeFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$WelcomeHomeToJson(this);
+}
+
+@JsonSerializable()
+class FloorList extends Object {
+  @JsonKey(name: 'curve')
+  int curve;
+
+  @JsonKey(name: 'content')
+  List<Content> content;
+
+  @JsonKey(name: 'bottomMargin')
+  int bottomMargin;
+
+  @JsonKey(name: 'isNewStyle')
+  int isNewStyle;
+
+  @JsonKey(name: 'showName')
+  String showName;
+
+  @JsonKey(name: 'isShadow')
+  int isShadow;
+
+  @JsonKey(name: 'type')
+  String type;
+
+  @JsonKey(name: 'slipTime')
+  int slipTime;
+
+  FloorList(
+    this.curve,
+    this.content,
+    this.bottomMargin,
+    this.isNewStyle,
+    this.showName,
+    this.isShadow,
+    this.type,
+    this.slipTime,
+  );
+
+  factory FloorList.fromJson(Map<String, dynamic> srcJson) =>
+      _$FloorListFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$FloorListToJson(this);
+}
+
+@JsonSerializable()
+class Content extends Object {
+  @JsonKey(name: 'extension_id')
+  String extensionId;
+
+  @JsonKey(name: 'activityId')
+  int activityId;
+
+  @JsonKey(name: 'sortno')
+  int sortno;
+
+  @JsonKey(name: 'abt')
+  String abt;
+
+  @JsonKey(name: 'title')
+  String title;
+
+  @JsonKey(name: 'wareDisplayType')
+  int wareDisplayType;
+
+  @JsonKey(name: 'horizontalImag')
+  String horizontalImag;
+
+  @JsonKey(name: 'sourceTag')
+  String sourceTag;
+
+  @JsonKey(name: 'exposalUrl')
+  String exposalUrl;
+
+  @JsonKey(name: 'clickUrl')
+  String clickUrl;
+
+  @JsonKey(name: 'jump')
+  Jump jump;
+
+  Content(
+    this.extensionId,
+    this.activityId,
+    this.sortno,
+    this.abt,
+    this.title,
+    this.wareDisplayType,
+    this.horizontalImag,
+    this.sourceTag,
+    this.exposalUrl,
+    this.clickUrl,
+    this.jump,
+  );
+
+  factory Content.fromJson(Map<String, dynamic> srcJson) =>
+      _$ContentFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$ContentToJson(this);
+}
+
+@JsonSerializable()
+class Jump extends Object {
+  @JsonKey(name: 'des')
+  String des;
+
+  @JsonKey(name: 'params')
+  Params params;
+
+  @JsonKey(name: 'srv')
+  String srv;
+
+  Jump(
+    this.des,
+    this.params,
+    this.srv,
+  );
+
+  factory Jump.fromJson(Map<String, dynamic> srcJson) =>
+      _$JumpFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$JumpToJson(this);
+}
+
+@JsonSerializable()
+class Params extends Object {
+  @JsonKey(name: 'url')
+  String url;
+
+  Params(
+    this.url,
+  );
+
+  factory Params.fromJson(Map<String, dynamic> srcJson) =>
+      _$ParamsFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$ParamsToJson(this);
+}
+
+@JsonSerializable()
+class WebViewFloorList extends Object {
+  @JsonKey(name: 'curve')
+  int curve;
+
+  @JsonKey(name: 'showTimes')
+  int showTimes;
+
+  @JsonKey(name: 'logoImage')
+  String logoImage;
+
+  @JsonKey(name: 'cornerDegree')
+  String cornerDegree;
+
+  @JsonKey(name: 'passthrough')
+  int passthrough;
+
+  @JsonKey(name: 'marginHorizontal')
+  int marginHorizontal;
+
+  @JsonKey(name: 'type')
+  String type;
+
+  @JsonKey(name: 'rightCorner')
+  String rightCorner;
+
+  @JsonKey(name: 'floorId')
+  int floorId;
+
+  @JsonKey(name: 'showName')
+  String showName;
+
+  @JsonKey(name: 'moduleFunction')
+  int moduleFunction;
+
+  @JsonKey(name: 'floorOrder')
+  int floorOrder;
+
+  @JsonKey(name: 'head')
+  int head;
+
+  @JsonKey(name: 'resultHeight')
+  int resultHeight;
+
+  @JsonKey(name: 'rightCornerImg')
+  String rightCornerImg;
+
+  @JsonKey(name: 'bottomMargin')
+  int bottomMargin;
+
+  @JsonKey(name: 'sourceValue')
+  String sourceValue;
+
+  @JsonKey(name: 'textColor')
+  String textColor;
+
+  @JsonKey(name: 'marginTop')
+  int marginTop;
+
+  @JsonKey(name: 'bottomMarginWidth')
+  int bottomMarginWidth;
+
+  @JsonKey(name: 'headType')
+  int headType;
+
+  @JsonKey(name: 'bottomMarginColor')
+  String bottomMarginColor;
+
+  @JsonKey(name: 'jump')
+  Jump jump;
+
+  @JsonKey(name: 'marginColor')
+  String marginColor;
+
+  @JsonKey(name: 'rightCornerColor')
+  String rightCornerColor;
+
+  @JsonKey(name: 'innnerInterval')
+  int innnerInterval;
+
+  @JsonKey(name: 'webViewList')
+  List<WebViewList> webViewList;
+
+  @JsonKey(name: 'refreshHeight')
+  int refreshHeight;
+
+  @JsonKey(name: 'verticalInterval')
+  int verticalInterval;
+
+  @JsonKey(name: 'animationTime')
+  int animationTime;
+
+  @JsonKey(name: 'tips')
+  String tips;
+
+  @JsonKey(name: 'bottomColor')
+  String bottomColor;
+
+  WebViewFloorList(
+    this.curve,
+    this.showTimes,
+    this.logoImage,
+    this.cornerDegree,
+    this.passthrough,
+    this.marginHorizontal,
+    this.type,
+    this.rightCorner,
+    this.floorId,
+    this.showName,
+    this.moduleFunction,
+    this.floorOrder,
+    this.head,
+    this.resultHeight,
+    this.rightCornerImg,
+    this.bottomMargin,
+    this.sourceValue,
+    this.textColor,
+    this.marginTop,
+    this.bottomMarginWidth,
+    this.headType,
+    this.bottomMarginColor,
+    this.jump,
+    this.marginColor,
+    this.rightCornerColor,
+    this.innnerInterval,
+    this.webViewList,
+    this.refreshHeight,
+    this.verticalInterval,
+    this.animationTime,
+    this.tips,
+    this.bottomColor,
+  );
+
+  factory WebViewFloorList.fromJson(Map<String, dynamic> srcJson) =>
+      _$WebViewFloorListFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$WebViewFloorListToJson(this);
+}
+
+@JsonSerializable()
+class Jump extends Object {
+  @JsonKey(name: 'params')
+  Params params;
+
+  @JsonKey(name: 'srv')
+  int srv;
+
+  Jump(
+    this.params,
+    this.srv,
+  );
+
+  factory Jump.fromJson(Map<String, dynamic> srcJson) =>
+      _$JumpFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$JumpToJson(this);
+}
+
+@JsonSerializable()
+class Params extends Object {
+  Params();
+
+  factory Params.fromJson(Map<String, dynamic> srcJson) =>
+      _$ParamsFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$ParamsToJson(this);
+}
+
+@JsonSerializable()
+class WebViewList extends Object {
+  @JsonKey(name: 'img')
+  String img;
+
+  @JsonKey(name: 'expo')
+  String expo;
+
+  @JsonKey(name: 'wordsColor')
+  String wordsColor;
+
+  @JsonKey(name: 'jump')
+  Jump jump;
+
+  WebViewList(
+    this.img,
+    this.expo,
+    this.wordsColor,
+    this.jump,
+  );
+
+  factory WebViewList.fromJson(Map<String, dynamic> srcJson) =>
+      _$WebViewListFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$WebViewListToJson(this);
+}
+
+@JsonSerializable()
+class Jump extends Object {
+  @JsonKey(name: 'des')
+  String des;
+
+  @JsonKey(name: 'shareInfo')
+  ShareInfo shareInfo;
+
+  @JsonKey(name: 'params')
+  Params params;
+
+  @JsonKey(name: 'srv')
+  int srv;
+
+  Jump(
+    this.des,
+    this.shareInfo,
+    this.params,
+    this.srv,
+  );
+
+  factory Jump.fromJson(Map<String, dynamic> srcJson) =>
+      _$JumpFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$JumpToJson(this);
+}
+
+@JsonSerializable()
+class ShareInfo extends Object {
+  ShareInfo();
+
+  factory ShareInfo.fromJson(Map<String, dynamic> srcJson) =>
+      _$ShareInfoFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$ShareInfoToJson(this);
+}
+
+@JsonSerializable()
+class Params extends Object {
+  @JsonKey(name: 'needLogin')
+  String needLogin;
+
+  @JsonKey(name: 'url')
+  String url;
+
+  Params(
+    this.needLogin,
+    this.url,
+  );
+
+  factory Params.fromJson(Map<String, dynamic> srcJson) =>
+      _$ParamsFromJson(srcJson);
+
+  Map<String, dynamic> toJson() => _$ParamsToJson(this);
+}
---
+++ lib/bean/home/welcome_home.g.dart
@@ -0,0 +1,293 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'welcome_home.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+WelcomeHome _$WelcomeHomeFromJson(Map<String, dynamic> json) {
+  return WelcomeHome(
+      json['naviVer'] as String,
+      json['toTopBtnImg'] as String,
+      json['configDisplayVersion'] as String,
+      json['userCategory'] as String,
+      json['executeTime'] as int,
+      json['topBgImgBig'] as String,
+      json['personalSourceValue'] as String,
+      json['popMessage'] as int,
+      json['searchBgColor'] as String,
+      json['displayVersion'] as String,
+      json['tagAnimations'] as int,
+      json['showLightIcon'] as int,
+      json['canBeDefault'] as bool,
+      json['loginText'] as String,
+      (json['floorList'] as List)
+          ?.map((e) =>
+              e == null ? null : FloorList.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      json['toBottomBtnImg'] as String,
+      json['cycNum'] as int,
+      json['use_new_brands'] as int,
+      json['alignedCurrentTime'] as int,
+      json['cycFirstTimeStamp'] as String,
+      json['searchBgImg'] as String,
+      json['code'] as String,
+      json['appCenterAnimations'] as int,
+      json['skinSwitch'] as int,
+      json['lottieCartoonList'] as List,
+      json['topIconText'] as int,
+      json['useComponentImg'] as int,
+      json['topBgImgBigColor'] as String,
+      json['appCenterCartoonSwitch'] as int,
+      json['topBgImg'] as String,
+      json['recommendAB'] as String,
+      json['lazy'] as int,
+      (json['webViewFloorList'] as List)
+          ?.map((e) => e == null
+              ? null
+              : WebViewFloorList.fromJson(e as Map<String, dynamic>))
+          ?.toList());
+}
+
+Map<String, dynamic> _$WelcomeHomeToJson(WelcomeHome instance) =>
+    <String, dynamic>{
+      'naviVer': instance.naviVer,
+      'toTopBtnImg': instance.toTopBtnImg,
+      'configDisplayVersion': instance.configDisplayVersion,
+      'userCategory': instance.userCategory,
+      'executeTime': instance.executeTime,
+      'topBgImgBig': instance.topBgImgBig,
+      'personalSourceValue': instance.personalSourceValue,
+      'popMessage': instance.popMessage,
+      'searchBgColor': instance.searchBgColor,
+      'displayVersion': instance.displayVersion,
+      'tagAnimations': instance.tagAnimations,
+      'showLightIcon': instance.showLightIcon,
+      'canBeDefault': instance.canBeDefault,
+      'loginText': instance.loginText,
+      'floorList': instance.floorList,
+      'toBottomBtnImg': instance.toBottomBtnImg,
+      'cycNum': instance.cycNum,
+      'use_new_brands': instance.useNewBrands,
+      'alignedCurrentTime': instance.alignedCurrentTime,
+      'cycFirstTimeStamp': instance.cycFirstTimeStamp,
+      'searchBgImg': instance.searchBgImg,
+      'code': instance.code,
+      'appCenterAnimations': instance.appCenterAnimations,
+      'skinSwitch': instance.skinSwitch,
+      'lottieCartoonList': instance.lottieCartoonList,
+      'topIconText': instance.topIconText,
+      'useComponentImg': instance.useComponentImg,
+      'topBgImgBigColor': instance.topBgImgBigColor,
+      'appCenterCartoonSwitch': instance.appCenterCartoonSwitch,
+      'topBgImg': instance.topBgImg,
+      'recommendAB': instance.recommendAB,
+      'lazy': instance.lazy,
+      'webViewFloorList': instance.webViewFloorList
+    };
+
+FloorList _$FloorListFromJson(Map<String, dynamic> json) {
+  return FloorList(
+      json['curve'] as int,
+      (json['content'] as List)
+          ?.map((e) =>
+              e == null ? null : Content.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      json['bottomMargin'] as int,
+      json['isNewStyle'] as int,
+      json['showName'] as String,
+      json['isShadow'] as int,
+      json['type'] as String,
+      json['slipTime'] as int);
+}
+
+Map<String, dynamic> _$FloorListToJson(FloorList instance) => <String, dynamic>{
+      'curve': instance.curve,
+      'content': instance.content,
+      'bottomMargin': instance.bottomMargin,
+      'isNewStyle': instance.isNewStyle,
+      'showName': instance.showName,
+      'isShadow': instance.isShadow,
+      'type': instance.type,
+      'slipTime': instance.slipTime
+    };
+
+Content _$ContentFromJson(Map<String, dynamic> json) {
+  return Content(
+      json['extension_id'] as String,
+      json['activityId'] as int,
+      json['sortno'] as int,
+      json['abt'] as String,
+      json['title'] as String,
+      json['wareDisplayType'] as int,
+      json['horizontalImag'] as String,
+      json['sourceTag'] as String,
+      json['exposalUrl'] as String,
+      json['clickUrl'] as String,
+      json['jump']);
+}
+
+Map<String, dynamic> _$ContentToJson(Content instance) => <String, dynamic>{
+      'extension_id': instance.extensionId,
+      'activityId': instance.activityId,
+      'sortno': instance.sortno,
+      'abt': instance.abt,
+      'title': instance.title,
+      'wareDisplayType': instance.wareDisplayType,
+      'horizontalImag': instance.horizontalImag,
+      'sourceTag': instance.sourceTag,
+      'exposalUrl': instance.exposalUrl,
+      'clickUrl': instance.clickUrl,
+      'jump': instance.jump
+    };
+
+Jump _$JumpFromJson(Map<String, dynamic> json) {
+  return Jump(json['des'] as String, json['params'], json['srv'] as String);
+}
+
+Map<String, dynamic> _$JumpToJson(Jump instance) => <String, dynamic>{
+      'des': instance.des,
+      'params': instance.params,
+      'srv': instance.srv
+    };
+
+Params _$ParamsFromJson(Map<String, dynamic> json) {
+  return Params(json['url'] as String);
+}
+
+Map<String, dynamic> _$ParamsToJson(Params instance) =>
+    <String, dynamic>{'url': instance.url};
+
+WebViewFloorList _$WebViewFloorListFromJson(Map<String, dynamic> json) {
+  return WebViewFloorList(
+      json['curve'] as int,
+      json['showTimes'] as int,
+      json['logoImage'] as String,
+      json['cornerDegree'] as String,
+      json['passthrough'] as int,
+      json['marginHorizontal'] as int,
+      json['type'] as String,
+      json['rightCorner'] as String,
+      json['floorId'] as int,
+      json['showName'] as String,
+      json['moduleFunction'] as int,
+      json['floorOrder'] as int,
+      json['head'] as int,
+      json['resultHeight'] as int,
+      json['rightCornerImg'] as String,
+      json['bottomMargin'] as int,
+      json['sourceValue'] as String,
+      json['textColor'] as String,
+      json['marginTop'] as int,
+      json['bottomMarginWidth'] as int,
+      json['headType'] as int,
+      json['bottomMarginColor'] as String,
+      json['jump'],
+      json['marginColor'] as String,
+      json['rightCornerColor'] as String,
+      json['innnerInterval'] as int,
+      (json['webViewList'] as List)
+          ?.map((e) => e == null
+              ? null
+              : WebViewList.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      json['refreshHeight'] as int,
+      json['verticalInterval'] as int,
+      json['animationTime'] as int,
+      json['tips'] as String,
+      json['bottomColor'] as String);
+}
+
+Map<String, dynamic> _$WebViewFloorListToJson(WebViewFloorList instance) =>
+    <String, dynamic>{
+      'curve': instance.curve,
+      'showTimes': instance.showTimes,
+      'logoImage': instance.logoImage,
+      'cornerDegree': instance.cornerDegree,
+      'passthrough': instance.passthrough,
+      'marginHorizontal': instance.marginHorizontal,
+      'type': instance.type,
+      'rightCorner': instance.rightCorner,
+      'floorId': instance.floorId,
+      'showName': instance.showName,
+      'moduleFunction': instance.moduleFunction,
+      'floorOrder': instance.floorOrder,
+      'head': instance.head,
+      'resultHeight': instance.resultHeight,
+      'rightCornerImg': instance.rightCornerImg,
+      'bottomMargin': instance.bottomMargin,
+      'sourceValue': instance.sourceValue,
+      'textColor': instance.textColor,
+      'marginTop': instance.marginTop,
+      'bottomMarginWidth': instance.bottomMarginWidth,
+      'headType': instance.headType,
+      'bottomMarginColor': instance.bottomMarginColor,
+      'jump': instance.jump,
+      'marginColor': instance.marginColor,
+      'rightCornerColor': instance.rightCornerColor,
+      'innnerInterval': instance.innnerInterval,
+      'webViewList': instance.webViewList,
+      'refreshHeight': instance.refreshHeight,
+      'verticalInterval': instance.verticalInterval,
+      'animationTime': instance.animationTime,
+      'tips': instance.tips,
+      'bottomColor': instance.bottomColor
+    };
+
+Jump _$JumpFromJson(Map<String, dynamic> json) {
+  return Jump(json['params'], json['srv'] as int);
+}
+
+Map<String, dynamic> _$JumpToJson(Jump instance) =>
+    <String, dynamic>{'params': instance.params, 'srv': instance.srv};
+
+Params _$ParamsFromJson(Map<String, dynamic> json) {
+  return Params();
+}
+
+Map<String, dynamic> _$ParamsToJson(Params instance) => <String, dynamic>{};
+
+WebViewList _$WebViewListFromJson(Map<String, dynamic> json) {
+  return WebViewList(json['img'] as String, json['expo'] as String,
+      json['wordsColor'] as String, json['jump']);
+}
+
+Map<String, dynamic> _$WebViewListToJson(WebViewList instance) =>
+    <String, dynamic>{
+      'img': instance.img,
+      'expo': instance.expo,
+      'wordsColor': instance.wordsColor,
+      'jump': instance.jump
+    };
+
+Jump _$JumpFromJson(Map<String, dynamic> json) {
+  return Jump(
+      json['des'] as String,
+      json['shareInfo'] == null
+          ? null
+          : ShareInfo.fromJson(json['shareInfo'] as Map<String, dynamic>),
+      json['params'],
+      json['srv'] as int);
+}
+
+Map<String, dynamic> _$JumpToJson(Jump instance) => <String, dynamic>{
+      'des': instance.des,
+      'shareInfo': instance.shareInfo,
+      'params': instance.params,
+      'srv': instance.srv
+    };
+
+ShareInfo _$ShareInfoFromJson(Map<String, dynamic> json) {
+  return ShareInfo();
+}
+
+Map<String, dynamic> _$ShareInfoToJson(ShareInfo instance) =>
+    <String, dynamic>{};
+
+Params _$ParamsFromJson(Map<String, dynamic> json) {
+  return Params(json['needLogin'] as String, json['url'] as String);
+}
+
+Map<String, dynamic> _$ParamsToJson(Params instance) =>
+    <String, dynamic>{'needLogin': instance.needLogin, 'url': instance.url};
