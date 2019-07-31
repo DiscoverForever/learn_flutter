40a7c30cce8adffdc43fbca03f2d54db37873d16
discoverforever <yueyangtang@163.com>
2019-07-31 17:21:07 +0800

    Mod 样式调整

--- lib/pages/home/item.dart
+++ lib/pages/home/item.dart
@@ -9,7 +9,6 @@ class ItemState extends State<Item> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 170,
       decoration: BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.all(Radius.circular(5)),
@@ -17,29 +16,88 @@ class ItemState extends State<Item> {
       child: Column(
         children: <Widget>[
           Container(
-            height: 150,
+            height: 185,
             width: double.infinity,
             child: Image.network(
               'http://img13.360buyimg.com/mobilecms/s372x372_jfs/t1/3405/18/3537/69901/5b997c0aE5dc8ed9f/a2c208410ae84d1f.jpg!q70.dpg.webp',
-              fit: BoxFit.cover,
+              fit: BoxFit.fitHeight,
             ),
           ),
           Container(
-            height: 20,
+            color: Colors.white,
             padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: Column(
               children: <Widget>[
-                Text(
-                  '¥100',
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontWeight: FontWeight.w700,
-                  ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        '自营',
+                        style: TextStyle(
+                          fontSize: 9,
+                          color: Colors.white,
+                        ),
+                      ),
+                      height: 15,
+                      padding: EdgeInsets.fromLTRB(4, 0, 4, 0),
+                      margin: EdgeInsets.fromLTRB(0, 0, 6, 0),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        '来自加利福尼亚的iphone xs max 原产于美国的空气 原产于美国的空气 原产于美国的空气',
+                        style: TextStyle(
+                          fontSize: 10,
+                          color: Colors.red,
+                        ),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
                 ),
-                FlatButton(
-                  onPressed: null,
-                  child: Text('看相似'),
+                Container(
+                  height: 25,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        '¥100',
+                        style: TextStyle(
+                          color: Colors.red,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      Container(
+                        height: 15,
+                        padding: EdgeInsets.fromLTRB(2, 0, 2, 0),
+                        child: Center(
+                          child: Text(
+                            '看相似',
+                            style: TextStyle(
+                              fontSize: 8,
+                            ),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.grey,
+                            width: 0.5,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(5),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
